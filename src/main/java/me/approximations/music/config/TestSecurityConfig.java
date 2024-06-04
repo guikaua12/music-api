d0f5cf8653609dcaaacbce041d0d72f7a0a6dfd3
Approximations <datkidbosst@gmail.com>
2024-06-04 15:45:21 -0300

    feat: added some UserController tests

--- src/main/java/me/approximations/music/config/TestSecurityConfig.java
+++ src/main/java/me/approximations/music/config/TestSecurityConfig.java
@@ -1,26 +1,20 @@
 package me.approximations.music.config;
 
 import me.approximations.music.handlers.Oauth2SuccessLoginHandler;
-import me.approximations.music.security.filters.JwtAuthenticationFilter;
 import me.approximations.music.security.handlers.SecurityAccessDeniedHandler;
 import me.approximations.music.security.handlers.SecurityAuthenticationEntryPoint;
-import me.approximations.music.security.jwt.JwtService;
-import me.approximations.music.services.user.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Profile("test")
 @Configuration
-@Profile("default")
-public class SecurityConfig {
+public class TestSecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http, Oauth2SuccessLoginHandler oauth2SuccessLoginHandler,
-                                           JwtService jwtService, CustomUserDetailsService userDetailsService) throws Exception {
-        http.addFilterBefore(new JwtAuthenticationFilter(jwtService, userDetailsService), UsernamePasswordAuthenticationFilter.class);
+    public SecurityFilterChain testFilterChain(HttpSecurity http, Oauth2SuccessLoginHandler oauth2SuccessLoginHandler) throws Exception {
         http.oauth2Login(c -> {
             c.successHandler(oauth2SuccessLoginHandler);
         });
---
+++ src/test/java/me/approximations/music/controllers/UserControllerTest.java
@@ -0,0 +1,45 @@
+package me.approximations.music.controllers;
+
+import me.approximations.music.entities.User;
+import me.approximations.music.entities.enums.AccountType;
+import me.approximations.music.security.authentication.JwtAuthenticationToken;
+import me.approximations.music.security.entities.CustomUserDetails;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@ActiveProfiles("test")
+public class UserControllerTest {
+    @Autowired
+    private UserController userController;
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void meShouldNotReturnUserIfUnauthenticated() throws Exception {
+        mockMvc.perform(get("/user/me")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    public void meShouldReturnUserIfAuthenticated() throws Exception {
+        final JwtAuthenticationToken auth = new JwtAuthenticationToken(new CustomUserDetails(new User(1L, "email", "name", AccountType.GOOGLE)), true);
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
+        mockMvc.perform(get("/user/me")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+    }
+
+}
